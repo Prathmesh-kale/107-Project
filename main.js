f4cad5025c675722133b715bec235bb5cdc07406
Prathmesh-kale <79739726+Prathmesh-kale@users.noreply.github.com>
2021-07-28 11:03:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+prediction1 = ""
+prediction2 = ""
+
+Webcam.set(
+    {
+        width : 350,
+        height : 300,
+        png_quality : 90,
+        img_format : 'png'
+    }
+);
+
+camera = document.getElementById("camera");
+
+Webcam.attach('camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function (data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'">';
+    })
+}
